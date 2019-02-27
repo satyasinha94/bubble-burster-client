ed2b6b35e6b831d78ebdd0e7da5dd406019832b9
satyasinha94 <satyasinha94@gmail.com>
2019-02-27 00:07:03 -0500

    basic charts

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
-import { BrowserRouter as Switch, Link, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Switch, NavLink, Route, Router } from 'react-router-dom';
 import {connect} from "react-redux"
 import {logout} from ".././Actions/AuthActions"
 import Artists from ".././Containers/Artists"
@@ -10,11 +10,11 @@ import Tracks from ".././Containers/Tracks"
 const NavBar = props => {
 
   return (
-      <Menu color="green">
-        <Menu.Item  as={Link} to="/tracks"  name= "Top Tracks" > Top Tracks </Menu.Item>
-        <Menu.Item  as={Link} to="/artists" name= "Top Artists"> Top Artists </Menu.Item>
-        <Menu.Item  as={Link} to="/genres" name= "Top Genres" > Top Genres </Menu.Item>
-        {props.auth.LoggedIn ? <Menu.Item name= "Logout" onClick = {props.logout}> Logout </Menu.Item> : null }
+      <Menu color="black">
+        <Menu.Item  as={NavLink} to="/tracks"  name= "Top Tracks" > Top Tracks </Menu.Item>
+        <Menu.Item  as={NavLink} to="/artists" name= "Top Artists"> Top Artists </Menu.Item>
+        <Menu.Item  as={NavLink} to="/genres" name= "Top Genres" > Top Genres </Menu.Item>
+        {props.auth.LoggedIn ? <Menu.Item  as={NavLink} to="/" name= "Logout" onClick = {props.logout}> Logout </Menu.Item> : null }
       </Menu>
   )
 }
--- src/Containers/Artists.js
+++ src/Containers/Artists.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { withRouter } from 'react-router-dom'
 import {connect} from "react-redux"
 import {getArtists} from ".././Actions/ArtistActions"
-import {bubbleChart} from '.././Components/BubbleChart'
+import { VictoryGroup, VictoryScatter, VictoryLabel } from 'victory'
 class Artists extends Component {
 
   state = {
@@ -19,7 +19,7 @@ class Artists extends Component {
 
   mapArtists = (artists) => {
     let mapArtistPopularity = artists.map(artist => {
-      let popularity = artist.relationships["user-artists"].data[0].popularity
+      let popularity = artist.relationships["user-artists"].data.find(d => d.username === this.props.user.username).popularity
       return {...artist, popularity: popularity}
     })
     this.setState({
@@ -29,14 +29,30 @@ class Artists extends Component {
 
   render(){
     return (
-      <svg id="viz">Artists</svg>
+      <VictoryGroup>
+        <VictoryScatter
+          style={{
+            data: { fill: "#c43a31" },
+            labels: {fontSize: 6} }
+          }
+          bubbleProperty="popularity"
+          maxBubbleSize={20}
+          minBubbleSize={2.5}
+          data={this.state.mappedArtists.map((artist, index) => {
+            return {x: Math.random(0,10000), y: Math.random(0,10000), popularity: artist.popularity}})
+          }
+          labels={this.state.mappedArtists.map(artist => `${artist.attributes.name} : ${artist.popularity}`)}
+
+        />
+        </VictoryGroup>
     )
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    artists: state.artists
+    artists: state.artists,
+    user: state.auth.user
     }
 }
 
